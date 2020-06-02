a6a1a582de8961d642a4f315f3c58411fa7b1f16
DennisVera <dennisvera.z@gmail.com>
2020-06-01 22:41:44 -0400

    Implement Movie View Model

---
+++ CococastsDemo/Extensions/UICollectionViewCell+Helpers.swift
@@ -0,0 +1,19 @@
+//
+//  UICollectionViewCell+Helpers.swift
+//  CococastsDemo
+//
+//  Created by Dennis Vera on 6/1/20.
+//  Copyright © 2020 Dennis Vera. All rights reserved.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+  
+  // MARK: - Properties
+  
+  static var nibName: String {
+    return String(describing: self)
+  }
+  
+}
--- CococastsDemo/Modules/Now Playing/View Controllers/NowPlayingViewController.swift
+++ CococastsDemo/Modules/Now Playing/View Controllers/NowPlayingViewController.swift
@@ -16,6 +16,18 @@ class NowPlayingViewController: UIViewController, Storyboardable {
     return "NowPlaying"
   }
   
+  @IBOutlet var collectionView: UICollectionView! {
+    didSet {
+      // Configure Collection View
+      collectionView.delegate = self
+      collectionView.dataSource = self
+      
+      // Register Movie Collection View Cell
+      let xib = UINib(nibName: MovieCollectionViewCell.nibName, bundle: .main)
+      collectionView.register(xib, forCellWithReuseIdentifier: MovieCollectionViewCell.reuseIdentifier)
+    }
+  }
+  
   // MARK: - Properties
   
   var viewModel: NowPlayingViewModel?
@@ -28,4 +40,64 @@ class NowPlayingViewController: UIViewController, Storyboardable {
     // Set Title
     title = "Now Playing"
   }
+  
+  // MARK: - View Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupViewModel()
+  }
+  
+  // MARK: - Helper Methods
+  
+  private func setupViewModel() {
+    // Install Handler
+    viewModel?.moviesDidChange = { [weak self] in
+      // Update Collection View
+      guard let strongSelf = self else { return }
+      strongSelf.collectionView.reloadData()
+    }
+  }
+}
+
+extension NowPlayingViewController: UICollectionViewDataSource {
+  
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return viewModel?.numberOfMovies ?? 0
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    // Fetch Movies
+    guard let movies = viewModel?.movies(at: indexPath.item) else { fatalError("No Movies Available.") }
+    
+    // Dequeue Movie Collection View Cell
+    let cell: MovieCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+    
+    // Configure Cell
+    cell.titleLabel.text = movies.title
+    
+    return cell
+  }
+}
+
+extension NowPlayingViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+    
+    return .init(width: collectionView.bounds.width, height: 340.0)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return 20.0
+  }
+  
 }
